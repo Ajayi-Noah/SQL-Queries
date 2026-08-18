dcdc3f27963dcb6d36f29747b0f213cbc85886f4
Ajayi-Noah <ajayibabson33@gmail.com>
2026-08-18 09:44:29 +0100

    Create MAIN DB.sql

---
+++ MAIN DB.sql
@@ -0,0 +1,29 @@
+-- 17/08/2026 || 1900Hr
+-------------------------------
+--CATEGORY PRICE RANKING
+-------------------------------
+SELECT 
+	P.name,
+	p.category,
+	P.price,
+ROUND(AVG(P.price) OVER(PARTITION BY P.category),2) as Price_average,
+DENSE_RANK () OVER(PARTITION BY P.category ORDER BY P.price DESC) AS Category_Price_ranking
+FROM products P;
+
+------------------------------------
+--MOST EXPENSIVE PRODUCT BY CATEGORY
+------------------------------------
+
+WITH CATEGORY_PRICE_RANKING AS (
+SELECT 
+	P.name,
+	p.category,
+	P.price,
+ROUND(AVG(P.price) OVER(PARTITION BY P.category),2) as Price_average,
+DENSE_RANK () OVER(PARTITION BY P.category ORDER BY P.price DESC) AS Category_Price_ranking
+FROM products P
+)
+
+SELECT *
+FROM CATEGORY_PRICE_RANKING CPR
+WHERE CPR.Category_Price_ranking = 1;
